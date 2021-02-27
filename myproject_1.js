8bf880e31a32c27666cd91c5ac0456aa10526c35
Kester <ilokester@gmail.com>
2021-02-27 10:27:50 +0100

    successfully randomly prints one message out of three

--- myproject_1.js
+++ myproject_1.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+
+const message1 = 'i love you right now';
+const message2 = 'i hate you right now';
+const message3 = 'i am blown right now';
+
+const messages = [message1, message2, message3];
+
+function openMessage() {
+    var randomnumber = Math.floor(Math.random() * 9);
+    return messages[randomnumber];
+}
+
+console.log(openMessage());
